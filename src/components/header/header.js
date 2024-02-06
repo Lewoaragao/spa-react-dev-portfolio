66cf0d85d5e62e651c760cbf3b6d417de8b1c1c5
Leonardo Aragão <leonardo.lca@isgh.org.br>
2024-02-06 16:00:23 -0300

    configurado para deploy no github pages

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,16 +1,17 @@
-// Importantando o React
-import React from "react";
 // Importando os components necessárias da lib react-materialize
-import { Navbar, NavItem, Row} from 'react-materialize';
+import { Navbar, Row } from "react-materialize";
 // Importando o Component NavLink da nossa lib de rotas
-import { NavLink } from 'react-router-dom'
-
+import { NavLink } from "react-router-dom";
 
 const Header = () => (
-  <Row>  
+  <Row>
     <Navbar className="grey darken-2">
-      <li><NavLink to="/">Home</NavLink></li>
-      <li><NavLink to="contact">Contact</NavLink></li>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="contact">Contact</NavLink>
+      </li>
     </Navbar>
   </Row>
 );
